827bdecceca61f4f1003d1686d326ee78b349766
plusplus1 <comeson@126.com>
2023-12-22 12:10:51 +0800

    feat: ver 21

--- utils4py/__init__.py
+++ utils4py/__init__.py
@@ -10,7 +10,7 @@ from utils4py.errors import SimpleError
 from utils4py.errors import overwrite_error_codes
 from utils4py.text import TextUtils
 
-__version__ = '0.1.20'
+__version__ = '0.1.21'
 
 _logger = logging.getLogger(__name__)
 
